af6a50e51b335a58adb6cf924073ae0fd5611346
Erick Agrazal <erick.agrazal@gmail.com>
2019-07-11 23:00:57 -0500

    Added docker-compose mongo.

--- ejemplo18.js.2/src/app.js
+++ ejemplo18.js.2/src/app.js
@@ -1,11 +1,44 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 const connectDb = require('./mongoose');
+const { User } = require('./models');
 
 const port = 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+// Enabling JSON bodyParser
+app.use(bodyParser());
+
+app.get('/', async (req, res) => {
+  res.json({ estudiantes: await User.find() });
+});
+
+app.get('/:id', async (req, res) => {
+  res.json(await User.findById(req.params.id));
+});
+
+app.get('/', async (req, res) => {
+  res.json({ estudiantes: await User.find() });
+});
+
+app.post('/', async (req, res) => {
+  const { name, age } = req.body;
+  await User.create({ name, age });
+  res.json({name, age});
+});
+
+app.put('/:id', async (req, res) => {
+  const { age } = req.body;
+  const estudiante = await User.findById(req.params.id);
+  estudiante.age = age;
+  estudiante.save();
+  res.json({age});
+});
+
+app.delete('/:id', async (req, res) => {
+  const estudiante = await User.findById(req.params.id);
+  estudiante.remove();
+  res.json({});
 });
 
 connectDb().then(() => {
